50b7e6756e8eac2c6ffe4af83c99044a0b3dfcbe
JokubasKul <jokubas.kulbokas@gmail.com>
2024-10-31 15:31:41 +0200

    commit 1

---
+++ Praktinis_darbas_3/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int first_crypt() {
+
+    int zodis,raktas;
+    string abecele[26]={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+  cout<<"Irasykite zodi ir rakta:"<<endl;
+  cin>>zodis>>raktas;
+
+
+
+
+
+}
+
+
+
+int main() {
+cout<<"---------------------------------------Ka noretumete daryti?---------------------------------------------"<<endl;
+    cout<<"1. Sifruoti su abcele"<<endl;
+    cout<<"2. Desifruoti su abecele"<<endl;
+    cout<<"3. Sifruoti su ASCII koduote"<<endl;
+    cout<<"4. Desifruoti su ASCII koduote"<<endl;
+
+
+
+
+    return 0;
+}
