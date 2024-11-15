5c5b76f1e647a7768c7591d8acc08a55b8ebe5f5
JokubasKul <jokubas.kulbokas@gmail.com>
2024-11-15 09:51:30 +0200

    commit2

--- Praktinis_darbas_3/main.cpp
+++ Praktinis_darbas_3/main.cpp
@@ -1,31 +1,149 @@
 #include <iostream>
+#include <fstream>
+#include <cstring>
+
 using namespace std;
 
+const char ABECELE[26] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
-int first_crypt() {
+void abeceleSifravimas() {
 
-    int zodis,raktas;
-    string abecele[26]={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-  cout<<"Irasykite zodi ir rakta:"<<endl;
-  cin>>zodis>>raktas;
-
+    char tekstas[50];
+    char rezultatas[50];
+    int poslinkis, tekstoIlgis, rezultatoIndeksas, k = 0;
 
 
+    cout<<"Parasykite teksta"<<endl;
+    cin>>tekstas;
+
+    tekstoIlgis = strlen(tekstas);
+    cout<<"Parasykite poslinki "<<"(turi buti nuo 1 iki 25)"<<endl;
+    cin>>poslinkis;
+
+
+    for(int i = 0; i < tekstoIlgis; i++) {
+        for(int j=0; j < sizeof(ABECELE); j++) {
+            if(toupper(tekstas[i]) == ABECELE[j]) {
+                rezultatoIndeksas = (j + poslinkis) % sizeof(ABECELE);
+                rezultatas[k] = ABECELE[rezultatoIndeksas];
+                k++;
+            }
+        }
+    }
+    cout<<endl;
+    cout<<rezultatas<<endl;
+
+}
+
+void abeceleDesifravimas() {
+
+    char tekstas[50];
+    char rezultatas[50];
+    int tekstoIlgis, poslinkis, rezultatoIndeksas, l=0;
+
+    cout<<"Irasykite teksta: "<<endl;
+    cin>>tekstas;
+
+    cout<<"Irasykite poslinki: (1-25)"<<endl;
+    cin>>poslinkis;
+
+    tekstoIlgis = strlen(tekstas);
+    for(int i = 0; i < tekstoIlgis; i++) {
+        for(int j=0; j < sizeof(ABECELE); j++) {
+            if(toupper(tekstas[i]) == ABECELE[j]) {
+                rezultatoIndeksas = (j - poslinkis + sizeof(ABECELE)) % sizeof(ABECELE);
+                rezultatas[l] = ABECELE[rezultatoIndeksas];
+                l++;
+            }
+        }
+    }
+    cout<<endl;
+    cout<<"Desifruotas tekstas: "<<rezultatas<<endl;
+
+}
+
+void ASCIIsifravimas() {
+
+    char tekstas[50];
+    char rezultatas[50];
+    int poslinkis, tekstoIlgis, rezultatoIndeksas, k=0;
+
+
+    cout<<"Parasykite teksta"<<endl;
+    cin>>tekstas;
+
+    tekstoIlgis = strlen(tekstas);
+
+    cout<<"Parasykite poslinki "<<"(turi buti nuo 1 iki 25)"<<endl;
+    cin>>poslinkis;
+
+    for(int i = 0; i < tekstoIlgis; i++) {
+            rezultatoIndeksas=(int(tekstas[i]) + poslinkis)%26;
+            rezultatas[k] = ABECELE[rezultatoIndeksas];
+            k++;
+    }
+
+cout<<rezultatas<<endl;
 
 
 }
 
+void ASCIIdesifravimas() {
+    char tekstas[50];
+    char rezultatas[50];
+    int poslinkis, tekstoIlgis, rezultatoIndeksas, k=0;
 
 
+    cout<<"Parasykite teksta"<<endl;
+    cin>>tekstas;
+
+    tekstoIlgis = strlen(tekstas);
+
+    cout<<"Parasykite poslinki "<<"(turi buti nuo 1 iki 25)"<<endl;
+    cin>>poslinkis;
+
+    for(int i = 0; i < tekstoIlgis; i++) {
+        rezultatoIndeksas=(int(tekstas[i]) - poslinkis + 26)%26;
+        rezultatas[k] = ABECELE[rezultatoIndeksas];
+        k++;
+    }
+
+    cout<<rezultatas<<endl;
+}
 int main() {
-cout<<"---------------------------------------Ka noretumete daryti?---------------------------------------------"<<endl;
-    cout<<"1. Sifruoti su abcele"<<endl;
-    cout<<"2. Desifruoti su abecele"<<endl;
-    cout<<"3. Sifruoti su ASCII koduote"<<endl;
-    cout<<"4. Desifruoti su ASCII koduote"<<endl;
+    int choice;
+    while(choice != 5) {
+        cout<<"----------------------------------------------------------"<<endl;
+        cout<<"1. Sifravimas su abecele "<<endl;
+        cout<<"2. Desifravimas su abecele "<<endl;
+        cout<<"3. Sifravimas su ASCII koduote "<<endl;
+        cout<<"4. Desifravimas su ASCII koduote "<<endl;
+        cout<<"5. Baigti cikla "<<endl;
+        cout<<endl;
+        cout<<"Irasykite pasirinkima (1-5)"<<endl;
+        cin>>choice;
 
-
-
+    switch(choice) {
+        case 1:
+            cout<<"Jus pasirinkote sifravima su abecele "<<endl;
+        abeceleSifravimas();
+        break;
+        case 2:
+            cout<<"Jus pasirinkote desifravima su abecele "<<endl;
+        abeceleDesifravimas();
+        break;
+        case 3:
+            cout<<"Jus pasirinkote sifravima su ASCII koduote "<<endl;
+        ASCIIsifravimas();
+        break;
+        case 4:
+            cout<<"Jus pasirinkote desifravima su ASCII koduote "<<endl;
+        ASCIIdesifravimas();
+        break;
+        default:
+            cout<<"Programa baigta "<<endl;
+    }
+}
 
     return 0;
 }
